293dd738fefd376a59b05b18821f70e2ba0c1cd3
shiku-mwethukia <marymwethukia65@gmail.com>
2022-06-29 14:36:36 +0300

    0-positive_or_negative.py git commit -m 0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+x = int(input("Please enter an integer: "))
+if x > 0:
+print("x:'is positive')
+elif x = 0:
+print("x:'is zero')
+else x < 0:
+print("x:'is negative')
