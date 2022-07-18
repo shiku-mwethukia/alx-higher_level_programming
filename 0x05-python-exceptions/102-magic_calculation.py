4d7054d0dbc1ab55a87645bcb684edf56b47b6da
shiku-mwethukia <marymwethukia65@gmail.com>
2022-07-19 02:19:25 +0300

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+        except Exception as error:
+            result = b + a
+            break
+    return result
