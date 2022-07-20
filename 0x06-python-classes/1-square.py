b0dcda1741e1aef6ea54c2783522088cc6b0e3ed
shiku-mwethukia <marymwethukia65@gmail.com>
2022-07-21 01:07:04 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square generating class for Python
+"""
+
+
+class Square:
+
+    """
+    A python class representing an empty square.
+    Declared with Instantiation with option size of int type.
+    """
+
+    def __init__(self, size):
+        """
+        Initializing the size data.
+        """
+        self.__size = size
