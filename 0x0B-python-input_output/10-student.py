9b935365aa2bd2def68c735c7f990f09709ce390
shiku-mwethukia <marymwethukia65@gmail.com>
2022-08-03 01:43:02 +0300

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A student class
+    """
+
+
+class Student:
+    """A class defining a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the student instance
+        Args:
+            first_name (string): Student first name
+            last_name (string): Student last name
+            age (int): Student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary format of the student instance
+        Args:
+            - attrs: list of attributes
+        """
+
+        my_dict = dict()
+        if type(attrs) is list and all(type(item) is str for item in attrs):
+            for item in attrs:
+                if item in self.__dict__:
+                    my_dict.update({item: self.__dict__[item]})
+            return my_dict
+        return self.__dict__.copy()
