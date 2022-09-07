914fed925c47b4e7ecd665c62c70b35fa07e83f0
shiku-mwethukia <marymwethukia65@gmail.com>
2022-09-08 02:06:47 +0300

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- a script that converts hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0
+   CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Changes the properties of a table in the database
+USE hbtn_0c_0;
+ALTER TABLE first_table
+   CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
