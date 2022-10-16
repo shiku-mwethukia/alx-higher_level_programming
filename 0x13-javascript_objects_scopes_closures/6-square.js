61b17b1005812f10f2a9c39d4365675241ff669e
shiku-mwethukia <marymwethukia65@gmail.com>
2022-10-16 23:07:51 +0300

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const SquareMaster = require('./5-square');
+
+class Square extends SquareMaster {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+    } else {
+      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
