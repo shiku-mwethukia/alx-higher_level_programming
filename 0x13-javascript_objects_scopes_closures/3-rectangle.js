3a5f13dab18d5c7acb01e827dbb29a9617dbad38
shiku-mwethukia <marymwethukia65@gmail.com>
2022-10-16 22:56:27 +0300

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+  }
+}
+module.exports = Rectangle;
