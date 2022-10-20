a6df65c9cd58b7d941ee77144184deab1cbe5236
shiku-mwethukia <marymwethukia65@gmail.com>
2022-10-20 09:50:52 +0300

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+script that changes the name of a State object
+from the database hbtn_0e_6_usa at id == 2
+'''
+
+
+if __name__ == '__main__':
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    state.name = 'New Mexico'
+
+    session.commit()
+
+    session.close()
